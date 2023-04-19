fee40eb6df4d9ecfea02149fe6273765419dad2c
ahadjonovss <samandarahadjonov@gmail.com>
2023-04-19 11:39:21 +0500

    project creating in progress

--- lib/utils/constants/app_icons.dart
+++ lib/utils/constants/app_icons.dart
@@ -1,4 +1,14 @@
 class AppIcons{
-  static const String iconPath = "assets/icons";
+  static const String iconPath = "assets/icons/";
+
+  static const bell = '${iconPath}bell.svg';
+  static const close = '${iconPath}close.svg';
+  static const marketing = '${iconPath}marketing.svg';
+  static const noTask = '${iconPath}not_task.svg';
+  static const party = '${iconPath}party.svg';
+  static const personal = '${iconPath}personal.svg';
+  static const shopping = '${iconPath}shopping.svg';
+  static const study = '${iconPath}study.svg';
+  static const work = '${iconPath}work.svg';
 
 }
