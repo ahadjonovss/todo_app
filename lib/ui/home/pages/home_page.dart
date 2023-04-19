cd9bb77346f40c61b6581d7ccf7c486943454c8f
ahadjonovss <samandarahadjonov@gmail.com>
2023-04-19 16:06:07 +0500

    project creating in progress

--- lib/ui/home/pages/home_page.dart
+++ lib/ui/home/pages/home_page.dart
@@ -10,8 +10,34 @@ class HomePage extends StatelessWidget {
         height: height(context),
         width: width(context),
         child: Column(
-          children: const  [
-            CustomAppBar()
+          children:   [
+            const CustomAppBar(),
+            Container(
+              width: width(context),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Today",style: AppTextStyles.headlineMedium(context),),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+                    child: Container(
+                      height: height(context)*0.06,
+                      width: width(context),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          left: BorderSide(
+                            color: Colors.black,
+                            width: 3.0,
+                          ),
+                        ),
+                        color: Colors.white
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
 
       ],
     ),
