a1141bfa113c595e429234661b29e970cb3dc595
ahadjonovss <samandarahadjonov@gmail.com>
2023-04-19 11:37:44 +0500

    project creating in progress

--- lib/utils/constants/app_images.dart
+++ lib/utils/constants/app_images.dart
@@ -1,5 +1,9 @@
 
 class AppImages{
-  static const String imagePath = "assets/images";
+  static const String imagePath = "assets/images/";
+
+  static const appLogo= '${imagePath}app_logo.png';
+  static const corner1= '${imagePath}corner1.png';
+  static const corner2= '${imagePath}corner2.png';
 
 }
