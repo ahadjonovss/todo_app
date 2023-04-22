137ff8af20833223e72bb24b1ed418c018b5a523
ahadjonovss <samandarahadjonov@gmail.com>
2023-04-22 07:18:14 +0500

    project creating in progress

---
+++ lib/ui/main/pages/categories/sub_screens/tasks_page.dart
@@ -0,0 +1,22 @@
+import 'package:todo_app/utils/tools/file_importer.dart';
+
+class TasksPage extends StatelessWidget {
+  List<List<TaskModel>> tasks;
+   TasksPage({required this.tasks,Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height(context),
+      width: width(context),
+      child: SingleChildScrollView(
+        child: Column(
+          children:  [
+            CustomAppBar(tasks:tasks[0]),
+            ...List.generate(tasks.length, (index) => TasksView(tasks: tasks[index],))
+          ],
+        ),
+      ),
+    );
+  }
+}
