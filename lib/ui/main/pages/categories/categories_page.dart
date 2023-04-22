b492fe4b613e6bac3e2b43786581fd80f2f7ca58
ahadjonovss <samandarahadjonov@gmail.com>
2023-04-22 07:00:32 +0500

    project creating in progress

--- lib/ui/main/pages/categories/categories_page.dart
+++ lib/ui/main/pages/categories/categories_page.dart
@@ -12,20 +12,40 @@ class _CategoriesPageState extends State<CategoriesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(tasks:context.read<TasksBloc>().state.tasks[0]),
-          Padding(
-            padding:  EdgeInsets.all(height(context)*0.02),
-            child: Text("Projects",style: AppTextStyles.headlineMedium(context),),
-          ),
-          Expanded(
-            child: GridView.count(crossAxisCount: 2,
+      body: BlocBuilder<TasksBloc, TasksState>(
+        builder: (context, state) {
+          return ListView(
             children: [
-              ...List.generate(categories.length, (index) => CategoryWidget(category: categories[index]))
-            ],),)
-        ],
+              CustomAppBar(tasks: context
+                  .read<TasksBloc>()
+                  .state
+                  .tasks[0]),
+              Padding(
+                padding: EdgeInsets.all(height(context) * 0.02),
+                child: Text(
+                  "Projects",
+                  style: AppTextStyles.headlineMedium(context),
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: GridView.count(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    crossAxisCount: 2,
+                    children: [
+                      ...List.generate(
+                        categories.length,
+                            (index) =>
+                            CategoryWidget(category: categories[index]),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
